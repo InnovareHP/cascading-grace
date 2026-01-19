b97c91a36a2d34e6bceac901fe4916df979252ca
Abc <cresencioabcdef26@gmail.com>
2026-01-19 23:26:39 +0800

    feat (main): removed watermark

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
-import { TanStackDevtools } from "@tanstack/react-devtools";
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { ThemeProvider } from "../components/theme-provider";
 
 import Footer from "../components/Footer";
@@ -98,17 +96,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
           {children}
           <Footer />
         </ThemeProvider>
-        <TanStackDevtools
-          config={{
-            position: "bottom-right",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
         <Scripts />
       </body>
     </html>
